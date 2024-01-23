2028e5384b6d492461ad54b02fa3c82a143def66
franck Chapelon <franckick2@gmail.com>
2024-01-23 21:37:30 +0100

    slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -39,7 +39,7 @@ const Slider = () => {
       initial: {
         opacity: 0,
         y: "-50%",
-        x: "40%",         // changé de 60% à 40%
+        x: "0%",         // changé de 60% à 0%
       },
       animate: {
         opacity: 1,
